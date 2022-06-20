2d91315e5c058cb7b17c5a4e58071592879faaf8
robin-swift <robinswiftart@gmail.com>
2022-06-20 16:16:07 +0100

    fixing qa_test after move

--- qa_test.sh
+++ qa_test.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-function c_build () {
-rm -fr build
-mkdir build
-cd build
-cmake ..
-make 2> build_error.log
-./embroider --test
-cd ..
-}
+python3 -m build
+python3 -m pip install dist/*.whl 
+python3 tests.py
 
-c_build
-
-#python3 -m build
-#python3 -m pip install dist/*.whl 
-#python3 tests.py
-
-#rm -fr build dist libembroidery/__pycache__ libembroidery/libembroidery.egg-info
+rm -fr build dist libembroidery/__pycache__ libembroidery/libembroidery.egg-info
